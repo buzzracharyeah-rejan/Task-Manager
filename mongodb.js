476806eec547a29e94631914ecc4a47b7e8dbf82
rejan buzzracharyeah <rejandev@gmail.com>
2021-11-17 13:43:28 +0545

    insert a single document with manually generated ObjectId

--- mongodb.js
+++ mongodb.js
@@ -1,31 +1,21 @@
-// CRUD Operation
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient: client, ObjectId } = require('mongodb');
 
-const connectionURL =
+const connectionURI =
   'mongodb+srv://test123:test123@cluster0.zzetb.mongodb.net/task-manager?retryWrites=true&w=majority';
 
-MongoClient.connect(
-  connectionURL,
-  { useNewUrlParser: true },
-  (error, client) => {
-    if (error) {
-      return console.log('Unable to connect to database');
-    }
-    const db = client.db('task-manager');
-
-    db.collection('users').insertOne(
-      {
-        name: 'rejan',
-        age: 22,
-      },
-      (error, result) => {
-        if (error) {
-          return console.log('unable to insert user');
-        }
-        console.log(result);
-        console.log(result.insertedId);
-      }
-    );
+client.connect(connectionURI, { useNewURLParser: true }, (error, client) => {
+  if (error) {
+    console.log('failed to connect to db');
   }
-);
+  const db = client.db('task-manager');
+  db.collection('users').insertOne(
+    {
+      _id: new ObjectId(),
+      name: 'rejan bajracharya',
+      age: 22,
+    },
+    (error, data) => {
+      console.log(data);
+    }
+  );
+});
