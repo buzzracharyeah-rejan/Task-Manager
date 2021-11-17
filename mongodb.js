4677f9de57d70c646898872e8adcdaccaf1f2149
rejan buzzracharyeah <rejandev@gmail.com>
2021-11-17 14:16:32 +0545

    downgrade mongodb version to support learning curve, use cursor to find docs in the collection

--- mongodb.js
+++ mongodb.js
@@ -3,19 +3,20 @@ const { MongoClient: client, ObjectId } = require('mongodb');
 const connectionURI =
   'mongodb+srv://test123:test123@cluster0.zzetb.mongodb.net/task-manager?retryWrites=true&w=majority';
 
-client.connect(connectionURI, { useNewURLParser: true }, (error, client) => {
+client.connect(connectionURI, { useUnifiedTopology: true }, (error, client) => {
   if (error) {
-    console.log('failed to connect to db');
+    return console.log('Failed to connect to the server');
   }
   const db = client.db('task-manager');
-  db.collection('users').insertOne(
-    {
-      _id: new ObjectId(),
-      name: 'rejan bajracharya',
-      age: 22,
-    },
-    (error, data) => {
-      console.log(data);
-    }
-  );
+  db.collection('users')
+    .find({ age: 22 })
+    .toArray((err, users) => {
+      console.log(users);
+    });
+
+  db.collection('users')
+    .find({ age: 22 })
+    .count((err, count) => {
+      console.log(count);
+    });
 });
